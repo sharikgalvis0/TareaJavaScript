efb1d91253bbe953675a48fc6d630908a9ebec5d
Sharik Galvis <sharikyeseniagalvisgarzon@pascualbravo.edu.co>
2024-09-04 22:43:52 -0500

    Update practica.js

--- practica.js
+++ practica.js
@@ -1,4 +1,35 @@
-let nombre = "Sharik"
-let apellido = "Galvis"
+// 10 variables con Let
+let nombre = "Sharik";
+let apellido = "Galvis";
+let edad= 18;
+let genero = "Femeninow"
+let estadoCivil = "Soltera";
+let eps = "SURA";
+let ciudad = "Medellín";
+let direccion = "Cra 43c # 41 A 52"
+let carrera = "Desarrollo de Software";
+let estadoMatricula = "Matriculado";
 
-console.log(nombre,apellido)
+// 5 variables con const
+
+const ID = 1061278380;
+const diaNacimiento = 17;
+const mesNacimiento = "Septiembre";
+const anioNacimiento = 2005;
+const tipoSangre = "O+"
+
+//OPERACIONES
+//Concatenación de dos maneras
+
+// 1
+let saludo = "Hola ";
+let mensajeCompleto = saludo + nombre + apellido + "¡BIENVENID@!";
+
+// 2
+let mcompleto =`Hola ${nombre} ${apellido} BIENVENID@`;
+
+
+
+
+
+console.log(mcompleto)
