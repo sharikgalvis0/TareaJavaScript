fb72b5a6470db95e05505caddccb161ccabeb206
Sharik Galvis <sharikyeseniagalvisgarzon@pascualbravo.edu.co>
2024-09-05 21:51:09 -0500

    Update practica.js

--- practica.js
+++ practica.js
@@ -19,17 +19,34 @@ const anioNacimiento = 2005;
 const tipoSangre = "O+"
 
 //OPERACIONES
+
 //Concatenación de dos maneras
 
 // 1
 let saludo = "Hola ";
 let mensajeCompleto = saludo + nombre + apellido + "¡BIENVENID@!";
+let nombreCompleto = nombre + " " + apellido;
+
+console.log(nombreCompleto)
+console.log(mensajeCompleto)
 
 // 2
 let mcompleto =`Hola ${nombre} ${apellido} BIENVENID@`;
-
-
-
-
+let informacionPersonal = `Nombre: ${nombreCompleto}, Edad: ${edad}, Ciudad: ${ciudad}, Carrera: ${carrera}`;
 
 console.log(mcompleto)
+console.log(informacionPersonal)
+
+// Validación (Operación lógica)
+// 1
+
+let validacionEdad = edad >= 18;
+console.log("Es mayor de edad:", validacionEdad);
+
+//OPERADOR TERNARIO
+
+let estadoCivill = (estadoCivil === "Soltera") ? "Estás soltera" : "No estás soltera";
+let estadoSalud = (eps === "SURA") ? "Asegurado con SURA" : "No asegurado con SURA";
+console.log(estadoSalud)
+console.log(estadoCivill)
+
